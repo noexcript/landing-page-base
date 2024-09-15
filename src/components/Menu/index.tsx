742ce7053c783d55d70f7b47ffab624d951eddee
Noah Magalhaes <noexcript@gmail.com>
2024-09-15 20:15:42 +0100

    feat: added better at scroll page

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,9 +3,20 @@ import { bar } from "../../lib/img"
 import { NavbarWrapper } from "../NavbarWrapper"
 import css from './index.module.css'
 import { useOpen } from "../../hook/useOpen"
+import { useEffect } from "react"
 
 export const Menu = () => {
-    const { setIsOpen } = useOpen()
+    const { setIsOpen, isOpen } = useOpen()
+
+    useEffect(() => {
+        if(isOpen){
+            document.body.style.overflow='hidden'
+            console.log('open')
+        }else{
+             document.body.style.overflow='auto'
+             console.log('close')
+        }
+    }, [isOpen])
     return (
         <>
             <button className={css.humburger} onClick={() => setIsOpen(true)}>
